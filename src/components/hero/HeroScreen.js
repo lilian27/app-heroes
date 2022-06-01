ed931d2acd4d51b2ce062a31d73abd4850754ad8
lilian patiño <lilian@MacBook-Air-de-lilian.local>
2022-05-31 21:20:45 -0400

    se generan componentes básicos de heores

---
+++ src/components/hero/HeroScreen.js
@@ -0,0 +1,61 @@
+import React, { useMemo } from 'react'
+import { Navigate, useParams, useNavigate } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById'
+
+export const HeroScreen = () => {
+  const { heroeId } = useParams();
+  const navigate = useNavigate()
+
+  const hero = useMemo(() => getHeroById(heroeId), [heroeId])
+
+  if (!hero) {
+    return <Navigate to='/' />
+  }
+
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero;
+
+  const imagePath = `/assets/image/${id}.jpg`;
+
+
+  const handleReturn = () => {
+    navigate(-1)
+  }
+
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={imagePath}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group">
+          <li className="list-group-item"><b>Alter ego:</b> {alter_ego}</li>
+          <li className="list-group-item"><b>Publisher:</b> {publisher}</li>
+          <li className="list-group-item"><b>First Appearance:</b> {first_appearance}</li>
+        </ul>
+
+        <h5>characters</h5>
+        <p>{characters}</p>
+
+        <button
+          className="btn btn-outline-info"
+          onClick={handleReturn}
+        >
+          Regresar
+        </button>
+      </div>
+    </div>
+  )
+}
